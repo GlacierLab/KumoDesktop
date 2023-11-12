7c4cd1663989f046172629216eb414506adc4349
琴梨梨 <24567775+qinlili23333@users.noreply.github.com>
2023-11-12 21:18:02 +1000

    实现下载功能

--- KumoNEXT/PackageManager.cs
+++ KumoNEXT/PackageManager.cs
@@ -1,6 +1,9 @@
 ﻿using KumoNEXT.Scheme;
+using KumoNEXT.Utils;
+using System.IO;
 using System.Net.Http;
 using System.Text.Json;
+using System.Windows.Threading;
 
 namespace KumoNEXT
 {
@@ -48,6 +51,11 @@ namespace KumoNEXT
                 }
                 return -101;
             }
+            if (Callback != null)
+            {
+                CallbackValue.Progress = 10;
+                Callback(CallbackValue);
+            }
             //确定需要安装的版本
             PkgOnline_PkgInfo Current = new() { PkgVersion = CheckInstall(ManifestParsed.Name) ? GetInstalledVersion(ManifestParsed.Name) : 0 };
             bool Found = false;
@@ -67,7 +75,39 @@ namespace KumoNEXT
             {
                 if (Current.Link.Length > 0)
                 {
+                    //找到需要安装的版本，开始安装
+                    var client = new HttpClient();
+                    var progress = new Progress<float>();
+                    progress.ProgressChanged += (_, e) =>
+                    {
+                        //回传下载进度
+                        if (Callback != null)
+                        {
+                            CallbackValue.Progress = 10+ (int)(e*0.8f);
+                            Callback(CallbackValue);
+                        }
+                    };
 
+                    //下载扩展包
+                    Directory.CreateDirectory("PackageCache");
+                    string FileName = "PackageCache\\" + ManifestParsed.Name + "." + Current.PkgVersion.ToString() + ".kumopkg";
+                    using (var file = new FileStream(FileName, FileMode.Create, FileAccess.Write, FileShare.None))
+                    {
+                        try
+                        {
+                            await client.DownloadDataAsync(Current.Link, file, progress);
+                        }catch (Exception)
+                        {
+                            //-102扩展包下载失败
+                            if (Callback != null)
+                            {
+                                CallbackValue.Progress = 10 + (int)(e * 0.8f);
+                                Callback(CallbackValue);
+                            }
+                            return -102;
+                        }
+                        return InstallFromFile(FileName);
+                    }
                 }
                 else
                 {
@@ -90,10 +130,14 @@ namespace KumoNEXT
                 }
                 return -200;
             }
-            return 0;
         }
         public static int InstallFromFile(string Path, Action<Scheme.PackageManagerInstallCallback>? Callback = null)
         {
+            Scheme.PackageManagerInstallCallback CallbackValue = new();
+            //读取包信息
+
+            //解压到目录
+
             return 0;
         }
         public static int Update(string PkgName)
