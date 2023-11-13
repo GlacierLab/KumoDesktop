fc2bb76927b5ed81afc3f291fc53d7a3608ee835
琴梨梨 <24567775+qinlili23333@users.noreply.github.com>
2023-11-13 23:38:29 +1000

    基本的PWA功能

--- KumoNEXT/TinyPWA.xaml.cs
+++ KumoNEXT/TinyPWA.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,6 +23,61 @@ namespace KumoNEXT
         public TinyPWA()
         {
             InitializeComponent();
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton ==MouseButton.Left) {
+                if (e.ClickCount == 2)
+                {
+                    WindowState = WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+                }
+                else
+                {
+                    if (WindowState == WindowState.Maximized)
+                    {
+                        double currentHeight = this.Height;
+                        double currentWidth = this.Width;
+                        double currentLeft = Left;
+                        double currentTop = Top;
+                        WindowState = WindowState.Normal;
+                        Left = currentLeft+10;
+                        Top= currentTop+10;
+                        this.Height= currentHeight-20;
+                        this.Width= currentWidth-20;
+                    }
+                    DragMove();
+                }
+            }
+        }
+
+        private void Window_StateChanged(object sender, EventArgs e)
+        {
+            if (this.WindowState == WindowState.Maximized)
+            {
+                WindowStyle = WindowStyle.SingleBorderWindow;
+                WindowStyle = WindowStyle.None;
+                this.BorderThickness = new Thickness(8);
+            }
+            else
+            {
+                this.BorderThickness = new Thickness(0);
+            }
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Maximize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+        }
+
+        private void Minimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
         }
     }
 }
