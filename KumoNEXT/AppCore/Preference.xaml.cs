72fe388dbe205b284a5058b8f9b900bfd5e6b563
琴梨梨 <24567775+qinlili23333@users.noreply.github.com>
2023-12-05 15:58:53 +0800

    渲染设置图标

--- KumoNEXT/AppCore/Preference.xaml.cs
+++ KumoNEXT/AppCore/Preference.xaml.cs
@@ -1,6 +1,11 @@
-﻿using System.Windows;
+﻿using Microsoft.WindowsAPICodePack.Taskbar;
+using System.Drawing;
+using System.IO;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Interop;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace KumoNEXT.AppCore
 {
@@ -18,11 +23,33 @@ namespace KumoNEXT.AppCore
             ParsedLocalData[0] = LocalData;
             Title = "设置-" + Manifest.DisplayName;
             //渲染设置图标
-            var SettingsIcon=this.FindResource("settings") as DrawingImage; DrawingVisual drawingVisual = new DrawingVisual();
+            var SettingsIcon=this.FindResource("settings") as DrawingImage; 
+            DrawingVisual drawingVisual = new DrawingVisual();
             using (DrawingContext drawingContext = drawingVisual.RenderOpen())
             {
                 drawingContext.DrawImage(SettingsIcon, new Rect(0, 0, 512, 512));
+                var PkgPath = "Package\\" + ParsedManifest.Name.Replace(".", "\\") + "\\";
+                if (File.Exists(PkgPath + "icon.png"))
+                {
+                    Stream imageStreamSource = new FileStream(PkgPath + "icon.png", FileMode.Open, FileAccess.Read, FileShare.Read);
+                    PngBitmapDecoder decoder = new PngBitmapDecoder(imageStreamSource, BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.Default);
+                    BitmapSource bitmapSource = decoder.Frames[0];
+                    drawingContext.DrawImage(bitmapSource, new Rect(192, 192, 320, 320));
+                }
+                else
+                {
+                    var bitmap = new BitmapImage(new Uri("pack://application:,,,/KumoNEXT;component/Res/Launch.png"));
+                    drawingContext.DrawImage(bitmap, new Rect(192, 192, 320, 320));
+                }
             }
+            RenderTargetBitmap bmp = new RenderTargetBitmap(512,512, 96, 96, PixelFormats.Pbgra32);
+            bmp.Render(drawingVisual);
+            Icon = bmp;
+            //设置AppID
+            SourceInitialized += (s, e) =>
+            {
+                TaskbarManager.Instance.SetApplicationIdForSpecificWindow(new WindowInteropHelper(this).Handle, "Kumo." + ParsedManifest.Name + "Settings");
+            };
         }
 
 
