ac66a913c13a73b7612c3fc798c739d07d5142a5
琴梨梨 <24567775+qinlili23333@users.noreply.github.com>
2023-12-21 17:44:18 +0800

    添加权限白名单

--- KumoNEXT/AppCore/PermissionManager.cs
+++ KumoNEXT/AppCore/PermissionManager.cs
@@ -1,15 +1,37 @@
 ﻿namespace KumoNEXT.AppCore
 {
-    public static class PermissionManager
+    public  class PermissionManager
     {
+
+        public PermissionManager(ref Scheme.PkgLocalData ParsedLocalData)
+        {
+        }
+
         public static bool CheckPackageManagePermission(string ContextPkg, string TargetPkg)
         {
             return TargetPkg.StartsWith(ContextPkg);
         }
 
-        public async static Task<bool> CheckAndRequestPermission(string ContextPkg, string Permission)
+        public async  Task<bool> CheckAndRequestPermission(string ContextPkg, string Permission)
         {
-            return false;
+            //先检查白名单
+            string[] Permissions;
+            if (PermissionWhitelist.TryGetValue(ContextPkg, out Permissions))
+            {
+                if (Permissions.Contains(Permission))
+                {
+                    return true;
+                }
+            }
+            //检查是否已经同意或拒绝过权限
+
+                return false;
         }
+
+        public static Dictionary<string, string[]> PermissionWhitelist=new Dictionary<string, string[]>()
+        {
+            { "CorePkg.Main",[ "ReadFile","WriteFile","Execute"]},
+            { "CorePkg.Update",[ "Package"]},
+        };
     }
 }
